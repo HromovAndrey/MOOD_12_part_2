aa54e66c23b1a9ac5c34770feaa430408ddb71c4
user <andrey.gromov.36912@gmail.com>
2024-04-21 19:04:01 +0300

    Лаба 2

--- Laba_0.py
+++ Laba_0.py
@@ -1,50 +1,38 @@
-# створіть програму що відтворює історію відвідувань веб сторінок
-# кожна відвідна сторінка додається до стеку
-# Можга реалізувати можливість повернуться  до попередньої сторінки
-# за допомогою видалення вершини стеку
+# 2 Завдання "Калькулятор"
+# Реалізуйте функцію, яка обчислює значення виразу, поданого у
+# польській(постфіксній) нотації. Використайте функцію з уроку
 
-class WebHistory:
-    def __init__(self):
-        self.stack = []
+def evaluate_expression(expression):
+    stack = []
 
-    def visit_page(self, page):
-        self.stack.append(page)
-        print(f"Відвідана сторінка: {page}")
-
-    def go_back(self):
-        if not self.stack:
-            print("Історія відвідувань порожня")
+    for token in expression.split():
+        if token.isdigit():
+            stack.append(int(token))
         else:
-            previous_page = self.stack.pop()
-            print(f"Повернення до попередньої сторінки: {previous_page}")
-def display_menu():
-    print("Меню:")
-    print("1. Відвідати нову сторінку")
-    print("2. Повернутися до попередньої сторінки")
-    print("3. Вийти")
-def main():
-    web_history = WebHistory()
+            try:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+            except IndexError:
+                raise ValueError("Invalid expression")
 
-    while True:
-        display_menu()
-        choice = input("Виберіть опцію: ")
+            if token == '+':
+                stack.append(operand1 + operand2)
+            elif token == '-':
+                stack.append(operand1 - operand2)
+            elif token == '*':
+                stack.append(operand1 * operand2)
+            elif token == '/':
+                stack.append(operand1 / operand2)
+            else:
+                raise ValueError("Invalid token: {}".format(token))
 
-        if choice == "1":
-            page = input("Введіть URL відвідуваної сторінки: ")
-            web_history.visit_page(page)
-        elif choice == "2":
-            web_history.go_back()
-        elif choice == "3":
-            print("До побачення!")
-            break
-        else:
-            print("Невідома опція. Спробуйте ще раз.")
-
-
-if __name__ == "__main__":
-    main()
-
-
+    if len(stack) == 1:
+        return stack[0]
+    else:
+        raise ValueError("Invalid expression")
+expression = "3 4 + 2 *"
+result = evaluate_expression(expression)
+print("Результат обчислення:", result)
 
 
 
